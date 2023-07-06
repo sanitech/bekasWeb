8b1345629fda8c9576d82b419387334b44086749
unknown <sanitech47@gmail.com>
2023-07-06 23:48:26 +0300

    second commit

---
+++ assets/js/data.js
@@ -0,0 +1,56 @@
+const product = [
+  {
+    productName: "products name",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A odit provident, velit consectetur deserunt mollitia pariatur quo dicta commodi praesentium, accusantium doloremque modi, unde molestiae harum aliquam vel animi at?",
+    category: "Bar Soap",
+    available: {
+      halfKg: "12pcs",
+      oneKg: "12pcs",
+      twoKg: "6pcs",
+      fourKg: "4pcs",
+      fiveKg: "4pcs",
+    },
+  },
+];
+const productCategories = [
+  "Liquid Detergents",
+  "Bar Soap Detergents",
+  "Cream and Abrasive",
+  "Industrial Surfactants",
+  "Cosmetic Products",
+  "Products for Construction",
+];
+
+const company = [
+  {
+    title: "About Us",
+    link: "#about",
+  },
+  {
+    title: "Contact Us",
+    link: "#contact",
+  },
+  {
+    title: "Services",
+    link: "#service",
+  },
+  {
+    title: "products",
+    link: "#product",
+  },
+  {
+    title: "News",
+    link: "#news",
+  },
+  {
+    title: "Mission and vision",
+    link: "#news",
+  },
+];
+const socialMedia = [
+  "bi bi-youtube",
+  "bi bi-facebook",
+  "bi bi-instagram",
+  "bi bi-tiktok",
+];
---
+++ plan.md
@@ -0,0 +1,1 @@
+order on products on website
